ff51019b6040a1a39e42747772d5d37fbab0f7ee
User-St <kasund28@gmail.com>
2021-04-07 22:30:12 +0530

    Blog v3

--- webfront/views.py
+++ webfront/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from accounts.models import Patient,Instructor
 
 #home page
 def index(request):
 
 
-    context = {
+    patients = Patient.objects.filter(user=request.user)
+    instructors = Instructor.objects.filter(user=request.user)
+
+    context = {'patients':patients, 'instructors' : instructors
 
     }
+
 
     return render(request, 'webfront/index.html', context)
